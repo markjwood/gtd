198f3225d6829dc275b6589b2b85b734ce0a47df
Mark <mark@markjwood.com>
2023-10-19 10:25:29 -0700

    add deadline

--- src/components/TodoList/AddTodoForm.jsx
+++ src/components/TodoList/AddTodoForm.jsx
@@ -1,30 +1,52 @@
 import { useState } from 'react';
 
 export default function AddTodoForm({ setTodos }) {
-	const [newTodo, setNewTodo] = useState('');
+	const [newTask, setNewTask] = useState('');
+	const [newDeadline, setNewDeadline] = useState('');
 
 	function handleSubmit(e) {
 		e.preventDefault();
-		if (newTodo.trim().length < 1 || !newTodo) return;
+		if (newTask.trim().length < 1 || !newTask) return;
 
-		const todo = { task: newTodo, id: crypto.randomUUID(), isComplete: false };
+		const todo = {
+			task: newTask,
+			id: crypto.randomUUID(),
+			deadline: newDeadline,
+			isComplete: false,
+		};
 
 		setTodos(prev => [...prev, todo]);
-		setNewTodo('');
+		setNewTask('');
 	}
 
 	return (
 		<form onSubmit={handleSubmit} className="flex flex-col gap-1">
-			<label className="text-xs mt-2 text-slate-800" htmlFor="newTodo">
-				New Todo Item
-			</label>
-			<input
-				className="p-1 border border-slate-600 rounded"
-				type="text"
-				id="newTodo"
-				value={newTodo}
-				onChange={e => setNewTodo(e.target.value)}
-			/>
+			<div className="flex gap-2">
+				<div className="flex flex-col grow">
+					<label className="text-xs mt-2 text-slate-800" htmlFor="newTask">
+						New Todo Item
+					</label>
+					<input
+						className="p-1 border border-slate-600 rounded"
+						type="text"
+						id="newTask"
+						value={newTask}
+						onChange={e => setNewTask(e.target.value)}
+					/>
+				</div>
+				<div className="flex flex-col">
+					<label className="text-xs mt-2 text-slate-800" htmlFor="newDeadline">
+						Deadline
+					</label>
+					<input
+						className="p-1 border border-slate-600 rounded"
+						type="date"
+						id="newDeadline"
+						value={newDeadline}
+						onChange={e => setNewDeadline(e.target.value)}
+					/>
+				</div>
+			</div>
 			<button className="mt-2 py-1 rounded hover:bg-cyan-100 transition bg-blue-200">
 				Submit
 			</button>
